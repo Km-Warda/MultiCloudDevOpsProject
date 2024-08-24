192ae9ea6e704c9c4df180b908c58923bc85a983
Karim Warda <109697567+Km-Warda@users.noreply.github.com>
2024-08-24 13:14:57 +0300

    Update variables.tf

--- Infrastructure Provisioning with Terraform/modules/VPC/variables.tf
+++ Infrastructure Provisioning with Terraform/modules/VPC/variables.tf
@@ -1,14 +1,11 @@
 variable "vpc_dns_support" {
     type = bool
-    default = true
 }
 variable "vpc_dns_hostnames" {
     type = bool
-    default = true
 }
 variable "vpc_cidr_block" {
     type = string
-    default = "10.0.0.0/16"
 }
 variable "vpc_tags" {
   type        = map(string)
