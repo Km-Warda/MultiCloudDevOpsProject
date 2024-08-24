d966cadb9b93ac024897edfed5965951f93ac9a0
Karim Warda <109697567+Km-Warda@users.noreply.github.com>
2024-08-24 13:14:09 +0300

    Update variables.tf

--- Infrastructure Provisioning with Terraform/modules/EC2/variables.tf
+++ Infrastructure Provisioning with Terraform/modules/EC2/variables.tf
@@ -3,11 +3,9 @@ variable "security_groups" {}
 
 variable "instance_ami" {
   type = string
-  default = "ami-0c55b159cbfafe1f0"
 }
 variable "instance_type" {
   type = string
-  default = "t2.micro"
 }
 variable "instance_tags" {
     type = map(string)
